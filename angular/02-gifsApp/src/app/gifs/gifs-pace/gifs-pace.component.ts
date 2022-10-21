26b68498ae6e08fa81e014c74283cdb67fe268d4
gtt6200 <gtt6200@gmail.com>
2022-10-21 00:54:49 -0600

    sidebar and gifsPage

---
+++ angular/02-gifsApp/src/app/gifs/gifs-pace/gifs-pace.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gifs-pace',
+  templateUrl: './gifs-pace.component.html',
+})
+export class GifsPaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular/02-gifsApp/src/app/gifs/gifs.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GifsPaceComponent } from './gifs-pace/gifs-pace.component';
+
+
+
+@NgModule({
+  declarations: [
+    GifsPaceComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GifsModule { }
