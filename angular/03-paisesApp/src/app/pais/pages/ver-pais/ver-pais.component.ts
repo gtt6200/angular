62753ce57204eacbae0e24a7ae3607938d5e19ea
gtt6200 <gtt6200@gmail.com>
2023-02-14 12:26:59 -0600

    complemetos seccion ver pais

--- angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ angular/03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+import { Country, Translations } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -9,23 +11,36 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!: Country;
+  traduccion : string[] = [];
   constructor( 
     private _activatedRoute : ActivatedRoute,
     private _paisService  : PaisService
     ) { }
 
   ngOnInit(): void {
-
+    
     this._activatedRoute.params
-    .subscribe( ({codigoPais}) =>{
-      console.log(codigoPais);
-      this._paisService.getCodigoPais(codigoPais)
-      .subscribe(pais =>{
-        console.log(pais);
+    .pipe(
+      switchMap( (param) => this._paisService.getCodigoPais(param['codigoPais']) )
+      ,tap(console.log)
+    )
+    .subscribe(pais => this.pais = pais);
+
+
+
+    // this._activatedRoute.params
+    // .subscribe( ({codigoPais}) =>{
+    //   console.log(codigoPais);
+    //   this._paisService.getCodigoPais(codigoPais)
+    //   .subscribe(pais =>{
+    //     console.log(pais);
         
-      })
-    } )
+    //   })
+    // } );
+    
 
   }
+  
 
 }
