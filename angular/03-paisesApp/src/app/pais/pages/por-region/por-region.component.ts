1a8b2e1b5ba30449899106fb6d0be896132118c0
gtt6200 <gtt6200@gmail.com>
2023-02-21 10:53:53 -0600

    por region

--- angular/03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ angular/03-paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styleUrls: ['./por-region.component.css']
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+
+  regiones: string[] = [  'EU', 'EFTA', 'CARICOM', 'PA',  'AU',  'USAN', 'EEU', 'AL', 'ASEAN',  'CAIS',  'CEFTA',  'NAFTA',  'SAARC', 
+];
+  regionActiva: string = '';
+
+
 
   constructor() { }
 
-  ngOnInit(): void {
+  getClaseCSS(region : string): string{
+    return (region === this.regionActiva) ? 'btn btn-success': 'btn btn-outline-success';
+  }
+
+  activarRegion (region:string){
+    this.regionActiva = region;
+    console.log(region);
+    
   }
 
 }
