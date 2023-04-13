0a7efcbc084ac1b062664ca782af0c3c4748ddb8
gtt6200 <gtt6200@gmail.com>
2023-04-13 12:33:56 -0600

    dinamicos component
    error, no muestra data en los inputs

--- angular/06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
+++ angular/06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this._formB.group({
+    nombre : [,[Validators.required, Validators.minLength(3)]],
+    favoritos: this._formB.array([
+      ['Smash', Validators.required], 
+      ['Mario Kart', Validators.required],
+      ['Mario Kart', Validators.required],
+      ['Mario Kart', Validators.required],
+    ], Validators.required)
+  });
 
+  
+  constructor( private _formB : FormBuilder) { }
+  
   ngOnInit(): void {
-  }
 
+  }
+  get favoritosArr(): FormArray {
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+  campoEsValido(campo:string){
+    return this.miFormulario.controls[campo].errors && 
+    this.miFormulario.controls[campo].touched;
+  }
+  guardar(){
+    if( this.miFormulario.invalid ){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+    console.log(this.miFormulario.value);
+    this.favoritosArr.clear();
+  }
+  
 }
+
+// miFormulario: FormGroup = this._formB.group({
+//   nombre : [,[Validators.required, Validators.minLength(3)]],
+//   favoritos: this._formB.group({
+//     opciones: this._formB.array<any>([
+//       ['Smash', Validators.required],
+//       ['Mario Party', Validators.required]
+//     ], Validators.required)
+//   })
+// });
